6049cd39387967333b84a2aa05bd0afb2d22a5e1
akashkr-1 <akashkr32858@gmail.com>
2026-03-13 09:34:41 +0530

    srtf.c

--- Lab1/srtf.c
+++ Lab1/srtf.c
@@ -1,67 +1,83 @@
 #include <stdio.h>
-#include <limits.h>
 
-void srtf(int n, int at[], int bt[]) {
+void srtf(int n, int at[], int bt[])
+{
     int rt[n], ct[n], tat[n], wt[n];
-    int completed = 0, current_time = 0;
+    int time = 0, done = 0;
     float total_wt = 0, total_tat = 0;
 
     for(int i = 0; i < n; i++)
         rt[i] = bt[i];
 
-    while(completed < n) {
-        int shortest = -1;
-        int min_rt = INT_MAX;
+    while(done < n)
+    {
+        int pos = -1;
+        int min = 9999;
 
-        for(int i = 0; i < n; i++) {
-            if(at[i] <= current_time && rt[i] > 0) {
-                if(rt[i] < min_rt || (rt[i] == min_rt && at[i] < at[shortest])) {
-                    min_rt = rt[i];
-                    shortest = i;
+        for(int i = 0; i < n; i++)
+        {
+            if(at[i] <= time && rt[i] > 0)
+            {
+                if(rt[i] < min)
+                {
+                    min = rt[i];
+                    pos = i;
                 }
             }
         }
 
-        if(shortest == -1) {
-            current_time++;
-        } else {
-            rt[shortest]--;
-            current_time++;
+        if(pos == -1)
+        {
+            time++;
+        }
+        else
+        {
+            rt[pos]--;
+            time++;
 
-            if(rt[shortest] == 0) {
-                completed++;
-                ct[shortest] = current_time;
-                tat[shortest] = ct[shortest] - at[shortest];
-                wt[shortest] = tat[shortest] - bt[shortest];
+            if(rt[pos] == 0)
+            {
+                done++;
 
-                total_wt += wt[shortest];
-                total_tat += tat[shortest];
+                ct[pos] = time;
+                tat[pos] = ct[pos] - at[pos];
+                wt[pos] = tat[pos] - bt[pos];
+
+                total_wt += wt[pos];
+                total_tat += tat[pos];
             }
         }
     }
 
     printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for(int i = 0; i < n; i++) {
+
+    for(int i = 0; i < n; i++)
+    {
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
+        i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
-    printf("\nAverage Waiting Time = %.2f", total_wt / n);
-    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
+    printf("\nAverage Waiting Time = %.2f", total_wt/n);
+    printf("\nAverage Turnaround Time = %.2f\n", total_tat/n);
 }
 
-int main() {
+int main()
+{
     printf("Name: AKASH K R\nUSN: 1BM24CS027\n");
+
     int n;
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
     int at[n], bt[n];
-    for(int i = 0; i < n; i++) {
+
+    for(int i = 0; i < n; i++)
+    {
         printf("Enter Arrival Time and Burst Time for P%d: ", i+1);
         scanf("%d %d", &at[i], &bt[i]);
     }
 
     srtf(n, at, bt);
+
     return 0;
 }
